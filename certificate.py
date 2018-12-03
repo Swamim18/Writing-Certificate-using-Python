a5f3728421878057acfb78f6868f4495e8b5c469
Swamim Saikia <31897267+Swamim18@users.noreply.github.com>
2018-12-03 11:04:47 +0530

    Update certificate.py

--- certificate.py
+++ certificate.py
@@ -8,6 +8,6 @@ draw.text(xy=(169,415),text="Sample Name",fill=(0,0,0),font=font_name)
 draw.text(xy=(75,510),text="for his cotribution of 400+ hours on open source projects in Java, Spring Boot",fill=(0,0,0),font=font_others)
 draw.text(xy=(320,720),text="TalentAccurate - 21 September 2018 - id: 10xZtTX",fill=(0,0,0),font=font_others)
 
-#image.save(r"C:\Users\swami\Desktop\certificate.png")
+image.save(r"C:\Users\swami\Desktop\certificate.png")
 
 image.show()
